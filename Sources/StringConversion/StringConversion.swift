82304bf5386195e784cab006cbb80656c08f60de
hristoh <hristo.hristov@mkodo.com>
2022-11-19 12:48:53 +0200

    P and Q conversions; tests

--- Sources/StringConversion/StringConversion.swift
+++ Sources/StringConversion/StringConversion.swift
@@ -1,6 +1,39 @@
-public struct StringConversion {
-    public private(set) var text = "Hello, World!"
+enum Conversion: Character {
+    case appendP = "P"
+    case reverseThenAppendQ = "Q"
+}
 
+extension String {
+    @discardableResult
+    mutating func removeLastConversion() -> Conversion? {
+        guard let last = last,
+              let conversion = Conversion(rawValue: last) else {
+            return nil
+        }
+        switch conversion {
+        case .appendP:
+            removeLast()
+        case .reverseThenAppendQ:
+            removeLast()
+            self = String(reversed())
+        }
+        return conversion
+    }
+}
+
+public struct StringConversion {
     public init() {
+    }
+    
+    static func can(_ initial: String, beConvertedInto target: String) -> Bool {
+        var beforeTransformations = target
+        while (beforeTransformations.count > initial.count &&
+               beforeTransformations != initial) {
+            let conversion = beforeTransformations.removeLastConversion()
+            if conversion == nil {
+                return false
+            }
+        }
+        return beforeTransformations == initial
     }
 }
--- Tests/StringConversionTests/StringConversionTests.swift
+++ Tests/StringConversionTests/StringConversionTests.swift
@@ -6,6 +6,21 @@ final class StringConversionTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(StringConversion().text, "Hello, World!")
+        XCTAssertEqual(
+            StringConversion.can("PQQ", beConvertedInto: "PQQP"),
+            true
+        )
+        XCTAssertEqual(
+            StringConversion.can("PQQ", beConvertedInto: "QQPQPPPP"),
+            true
+        )
+        XCTAssertEqual(
+            StringConversion.can("P", beConvertedInto: "PQPQQQ"),
+            false
+        )
+        XCTAssertEqual(
+            StringConversion.can("PQQ", beConvertedInto: "PQQ"),
+            true
+        )
     }
 }
